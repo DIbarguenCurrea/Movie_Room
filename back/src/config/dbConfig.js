9e21c8fa71d23210b074fc4fde369994306fbd2e
DIbarguenCurrea <dibarguenc@gmail.com>
2024-06-10 18:26:41 +0200

    Update Files

--- back/src/config/dbConfig.js
+++ back/src/config/dbConfig.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConfig = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(process.env.MONGO_URI);
-}
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log(process.env.MONGO_URI);
+};
 
 module.exports = dbConfig;
