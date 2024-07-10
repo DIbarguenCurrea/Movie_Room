38ef024a332f728c9cc3d257f8d648fa7a0376d1
DIbarguenCurrea <dibarguenc@gmail.com>
2024-07-10 17:28:24 +0200

    Update

--- back/README.md
+++
@@ -1,7 +0,0 @@
-Versión: 01 
-
-Status: 
-
-Se realizar la creacion de la carpeta SRC y en ella se crea el archivo server.js y las subcarpetas Controller, Routes y Services. 
-
-Se trabaja primero en el archivo server.js, trayendo la libreria de express, instacinamos el express y luego lo exportamos. 
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,23 +1,21 @@
 const createMovie = require("./createMovie");
 const cardGenerator = require("./renderCards");
 // const URL = "https://students-api.2.us-1.fl0.io/movies";
-const API_URI = proccess.env.API_URI;
-const API_KEY = proccess.env.API_KEY;
-
-const API_URL = `${API_URI}${API_KEY}`;
+const URL = "http://localhost:3000/movies";
 const axios = require("axios");
 
 const getMovies = async () => {
-  try {
-    const { data } = await axios.get(API_URI);
-    data.map((movie) => cardGenerator(movie));
-  } catch ({ message }) {
-    alert(message);
-  }
-};
+    try { 
+        const { data } = await axios.get (URL);
+        data.map(movie => cardGenerator(movie));
+    } catch ({ message }) {
+        alert(message);
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  getMovies();
-  const submit = document.getElementById("formCreateMovie");
-  submit?.addEventListener("submit", createMovie);
+    getMovies();
+    const submit = document.getElementById("formCreateMovie");
+    submit?.addEventListener("submit", createMovie);  
+
 });
